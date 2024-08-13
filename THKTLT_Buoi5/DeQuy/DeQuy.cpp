0ca02be165a7194125340fe3449c5291960ea918
phongdeptrais1tg <hoanganhphong2002hcm@gmail.com>
2024-08-13 17:35:40 +0700

    Buoi5_Bai2.2

--- THKTLT_Buoi5/DeQuy/DeQuy.cpp
+++ THKTLT_Buoi5/DeQuy/DeQuy.cpp
@@ -6,6 +6,11 @@ int sum(int n) {
         return 1;
     return n + sum(n - 1);
 }
+int sumArray(int arr[], int n) {
+    if (n <= 0)
+        return 0;
+    return arr[n - 1] + sumArray(arr, n - 1);
+}
 int main() {
     int choice, n, x;
     int arr[100];
@@ -14,7 +19,7 @@ int main() {
     while (1) {
         printf("\nMenu\n");
         printf("1. Tinh tong tu 1 den n\n");
-        
+        printf("2. Tinh tong mang co n phan tu\n");
         printf("6. Thoat\n");
         printf("Chon mot tuy chon: ");
         scanf("%d", &choice);
@@ -25,7 +30,15 @@ int main() {
             scanf("%d", &n);
             printf("Tong tu 1 den %d la %d\n", n, sum(n));
             break;
-        
+        case 2:
+            printf("Nhap n: ");
+            scanf("%d", &n);
+            printf("Nhap cac phan tu cua mang:\n");
+            for (int i = 0; i < n; i++) {
+                scanf("%d", &arr[i]);
+            }
+            printf("Tong cua mang la %d\n", sumArray(arr, n));
+            break;
         case 6:
             return 0;
         default:
